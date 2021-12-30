7ec3fc522ef85fdf529d9e0367e251d444b7fd2a
ani_shres <anisha.shrestha@hotmail.com>
2021-12-30 17:51:54 +1100

    joke generator completed

--- src/App.js
+++ src/App.js
@@ -1,22 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import useRandomJoke from "./useRandomJoke";
+import { useRef, useState } from "react";
 
 function App() {
+  const firstNameRef = useRef(null);
+  const lastNameRef = useRef(null);
+
+  const [firstName, setFirstName] = useState("Steve");
+  const [lastName, setLastName] = useState("Jobs");
+
+  const joke = useRandomJoke(firstName, lastName);
+
+  const generateJoke = (e) => {
+    e.preventDefault(); // prevent refresh
+    setFirstName(firstNameRef.current.value);
+    setLastName(lastNameRef.current.value);
+  };
+  //Custome Hook
+
+  //const joke = useRandomJoke("Anisha","shrestha")
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Joke Generator</h1>
+        <h2 className="App--joke">{joke}</h2>
+        <form className="App--form">
+          <input
+            className="App--form--firstname"
+            placeholder="first name"
+            ref={firstNameRef}
+            // value={firstName}
+            // onChange={(e) => e.target.value}  //creates new request every single time
+          />
+          <input
+            className="App--form--lastname"
+            placeholder="last name"
+            ref={lastNameRef}
+            // value={lastName}
+            // onChange={(e) => e.target.value}
+          />
+        </form>
+        <button className="App--header--button" onClick={generateJoke}>
+          generate joke
+        </button>
       </header>
     </div>
   );
